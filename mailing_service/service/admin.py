284103d02aafa003c7a26c46cb3333d30e61b04e
Saidova <saidova.kseniya@mail.ru>
2025-10-26 13:51:59 +0300

    Регистрация модели в админке (service/admin.py)

--- mailing_service/service/admin.py
+++ mailing_service/service/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Client # Импортируем нашу модель
+from .models import Client, Message
 
 # Регистрируем модель для отображения в админке
 admin.site.register(Client)
+admin.site.register(Message)
