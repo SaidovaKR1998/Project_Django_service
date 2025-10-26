545f7a619347f99bd984a3dd11d79a7694a3b888
Saidova <saidova.kseniya@mail.ru>
2025-10-26 13:48:57 +0300

    Управление сообщениями (Модель "Message")

--- mailing_service/service/models.py
+++ mailing_service/service/models.py
@@ -11,3 +11,14 @@ class Client(models.Model):
     class Meta:
         verbose_name = 'Клиент'
         verbose_name_plural = 'Клиенты'
+
+class Message(models.Model):
+    subject = models.CharField(max_length=255, verbose_name='Тема письма')
+    body = models.TextField(verbose_name='Тело письма')
+
+    def __str__(self):
+        return self.subject
+
+    class Meta:
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщения'
