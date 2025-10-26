2e6f1f30c327319bbcf77f5751d1b39cf124a8be
Saidova <saidova.kseniya@mail.ru>
2025-10-26 14:03:58 +0300

    Создание файла service/urls.py

---
+++ mailing_service/service/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = 'service'
+
+urlpatterns = [
+    path('', views.index, name='home'), # Главная страница будет по адресу http://127.0.0.1:8000/
+]
