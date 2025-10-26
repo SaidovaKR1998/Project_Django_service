0e2ddd02d574cf94258e9984146071a19ad65790
Saidova <saidova.kseniya@mail.ru>
2025-10-26 14:39:41 +0300

    Добавлен URL для статистики в service/urls.py:

--- mailing_service/service/urls.py
+++ mailing_service/service/urls.py
@@ -5,4 +5,5 @@ app_name = 'service'
 
 urlpatterns = [
     path('', views.index, name='home'), # Главная страница будет по адресу http://127.0.0.1:8000/
+    path('statistics/', views.statistics, name='statistics'),
 ]
