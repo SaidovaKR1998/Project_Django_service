6619f009500fa3f81f3ac1c60c5dcdbed1097a90
Saidova <saidova.kseniya@mail.ru>
2025-10-26 13:42:49 +0300

    Управление клиентами (Модель "Получатель")

--- mailing_service/service/models.py
+++ mailing_service/service/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    email = models.EmailField(unique=True, verbose_name='Электронная почта')
+    full_name = models.CharField(max_length=255, verbose_name='ФИО')
+    comment = models.TextField(blank=True, null=True, verbose_name='Комментарий')
+
+    def __str__(self):
+        return f"{self.full_name} ({self.email})"
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
