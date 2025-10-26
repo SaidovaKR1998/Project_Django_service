b45e4e9afe73debdf6e28e19b03710f916384b93
Saidova <saidova.kseniya@mail.ru>
2025-10-26 14:18:02 +0300

    Настройка кастомной модели пользователя в настройках. В mailing_service/settings.py

--- mailing_service/mailing_service/settings.py
+++ mailing_service/mailing_service/settings.py
@@ -131,3 +131,14 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # EMAIL_HOST = 'smtp.yandex.ru'
 # EMAIL_PORT = 465
 # ... и т.д.
+
+AUTH_USER_MODEL = 'users.User'
+
+# Настройки для email
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # Для разработки
+DEFAULT_FROM_EMAIL = 'noreply@mailingservice.com'
+
+# Настройки аутентификации
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = '/users/login/'
