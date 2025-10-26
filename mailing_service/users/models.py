5bf7eee523bee20392e5f9923a7f77d8c5ffb799
Saidova <saidova.kseniya@mail.ru>
2025-10-26 14:16:29 +0300

    Настройка приложения users Создадим модель пользователя с дополнительными полями users/models.py

--- mailing_service/users/models.py
+++ mailing_service/users/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, verbose_name='Электронная почта')
+    email_verified = models.BooleanField(default=False, verbose_name='Email подтвержден')
+
+    # Добавляем related_name чтобы избежать конфликтов
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        related_name="custom_user_set",
+        related_query_name="user",
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='user permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_name="custom_user_set",
+        related_query_name="user",
+    )
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
