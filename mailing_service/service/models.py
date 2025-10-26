ced0419c73b7da41491c436f1a71dadeece8695e
Saidova <saidova.kseniya@mail.ru>
2025-10-26 14:31:15 +0300

    Обновление модели в service/models.py Добавление владельца для всех моделей

--- mailing_service/service/models.py
+++ mailing_service/service/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.conf import settings
 
 class Client(models.Model):
     email = models.EmailField(unique=True, verbose_name='Электронная почта')
     full_name = models.CharField(max_length=255, verbose_name='ФИО')
     comment = models.TextField(blank=True, null=True, verbose_name='Комментарий')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Владелец', null=True, blank=True)
 
     def __str__(self):
         return f"{self.full_name} ({self.email})"
@@ -11,10 +13,14 @@ class Client(models.Model):
     class Meta:
         verbose_name = 'Клиент'
         verbose_name_plural = 'Клиенты'
+        permissions = [
+            ("view_all_clients", "Может просматривать всех клиентов"),
+        ]
 
 class Message(models.Model):
     subject = models.CharField(max_length=255, verbose_name='Тема письма')
     body = models.TextField(verbose_name='Тело письма')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Владелец', null=True, blank=True)
 
     def __str__(self):
         return self.subject
@@ -35,6 +41,7 @@ class Mailing(models.Model):
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='created', verbose_name='Статус')
     message = models.ForeignKey(Message, on_delete=models.CASCADE, verbose_name='Сообщение')
     clients = models.ManyToManyField(Client, verbose_name='Клиенты')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Владелец', null=True, blank=True)
 
     def __str__(self):
         return f"Рассылка {self.id} ({self.status})"
