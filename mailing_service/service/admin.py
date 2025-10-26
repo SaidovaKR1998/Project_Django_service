8418d0d85f4e3149bbff0c59c496b16d4c31ede7
Saidova <saidova.kseniya@mail.ru>
2025-10-26 13:54:34 +0300

    Зарегистрируем модель в админке (service/admin.py)

--- mailing_service/service/admin.py
+++ mailing_service/service/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Client, Message
+from .models import Client, Message, Mailing
 
 # Регистрируем модель для отображения в админке
 admin.site.register(Client)
 admin.site.register(Message)
+admin.site.register(Mailing)
