dc620dc9b769830801c07b46635956511e2464ed
Saidova <saidova.kseniya@mail.ru>
2025-10-26 14:04:40 +0300

    Создание представления (view) для главной страницы service/views.py

--- mailing_service/service/views.py
+++ mailing_service/service/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Mailing, Client
 
-# Create your views here.
+def index(request):
+    # Считаем статистику
+    total_mailings = Mailing.objects.all().count()
+    active_mailings = Mailing.objects.filter(status='started').count()
+    unique_clients = Client.objects.distinct().count()
+
+    context = {
+        'total_mailings': total_mailings,
+        'active_mailings': active_mailings,
+        'unique_clients': unique_clients,
+    }
+    return render(request, 'service/index.html', context)
